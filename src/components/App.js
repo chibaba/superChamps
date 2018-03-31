d5a4e4c5c3eea05ab1417222f5fd480c10dd8713
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2018-03-31 20:31:18 +0100

    redux commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import CharacterList from './CharacterList';
+import HeroList from './HeroList'; 
+import '../styles/index.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App"> 
+        <h2>superSuad</h2>
+        <div className="col-md-6">
+        <CharacterList />
+        </div>
+        <div className='col-md-6'>
+        <HeroList />
+        </div>
+      </div>
+    )
+  }
+}
+export default App;
